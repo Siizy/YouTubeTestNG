71ed56739f99bab4849b4d025f8aab1dc26df2d0
ChandanGupta <ChandanGupta@INDP277LT.tsys.tss.net>
2021-01-04 23:37:16 +0530

    Added code used in Cross Browser Testing

---
+++ YTTestNGTutorials/src/crossbrowser/Class01.java
@@ -0,0 +1,63 @@
+package crossbrowser;
+
+import java.io.IOException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+
+public class Class01 {
+	
+	WebDriver driver;
+	
+	@BeforeMethod
+	@Parameters("browser")
+	public void init(String browser){
+		
+		switch (browser.toLowerCase()) {
+
+		case "chrome":
+			System.setProperty("webdriver.chrome.driver", "drivers//chromedriver.exe");
+			driver = new ChromeDriver();			
+			break;
+			
+		case "ie":
+			System.setProperty("webdriver.ie.driver", "drivers//IEDriverServer.exe");
+			driver = new InternetExplorerDriver();			
+			break;
+			
+		case "firefox":
+			System.setProperty("webdriver.gecko.driver", "drivers//geckodriver.exe");
+			driver = new FirefoxDriver();			
+			break;
+
+		default:
+			driver = null;
+			break;
+		}
+						
+	}
+	
+	@Test
+	public void verifyPageTitle() {     
+		driver.get("https://www.google.com");
+		Assert.assertEquals(driver.getTitle(), "Google");
+	}
+			
+	@AfterMethod
+	public void tearDown() throws IOException{     
+		driver.quit();	
+		Runtime.getRuntime().exec("taskkill /F /IM chromedriver.exe");
+	}
+}
---
+++ YTTestNGTutorials/src/invocationcount/InvocationClass01.java
@@ -0,0 +1,11 @@
+package invocationcount;
+
+import org.testng.annotations.Test;
+
+public class InvocationClass01 {
+			
+	@Test(invocationCount = 6, threadPoolSize = 3)
+	public void Test01() {		
+		System.out.println("I am in Test01 - Thread ID = " + Thread.currentThread().getId() );			
+	}			
+}
