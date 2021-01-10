c5f196d035d5d879cf335cb3480054585e24c4f8
ChandanGupta <CG>
2021-01-10 09:15:22 +0530

    Added listener code

--- YTTestNGTutorials/src/parallel/ParallelClass01.java
+++ YTTestNGTutorials/src/parallel/ParallelClass01.java
@@ -9,12 +9,14 @@ import org.testng.annotations.Test;
 
 public class ParallelClass01 {
 	
+	WebDriver driver;
+	
 	//A thread is an independent path of execution within a program		  
 	@Test
 	public void test01() {		
 		System.out.println("I am in test01_Class01 - ID: " + Thread.currentThread().getId());
 		System.setProperty("webdriver.chrome.driver", "drivers\\chromedriver.exe");	
-		WebDriver driver = new ChromeDriver();
+		driver = new ChromeDriver();
 		driver.manage().window().setPosition(new Point(1500,0));		
 		driver.get("https://www.google.com/docs/about/");
 	}
@@ -23,7 +25,7 @@ public class ParallelClass01 {
 	public void test02() {		
 		System.out.println("I am in test02_Class01 - ID: " + Thread.currentThread().getId());
 		System.setProperty("webdriver.chrome.driver", "drivers\\chromedriver.exe");	
-		WebDriver driver = new ChromeDriver();
+		driver = new ChromeDriver();
 		driver.manage().window().setPosition(new Point(1500,0));		
 		driver.get("https://www.google.com");
 	}
