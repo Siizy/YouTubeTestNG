8296b23e965a31a9705f9847f7d455c09ddb097b
ChandanGupta <ChandanGupta@INDP277LT.tsys.tss.net>
2021-01-12 18:06:53 +0530

    Added code for ISuiteListener

--- YTTestNGTutorials/src/invocationcount/InvocationClass01.java
+++ YTTestNGTutorials/src/invocationcount/InvocationClass01.java
@@ -1,5 +1,6 @@
 package invocationcount;
 
+import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
 
 public class InvocationClass01 {
@@ -7,5 +8,5 @@ public class InvocationClass01 {
 	@Test(invocationCount = 6, threadPoolSize = 3)
 	public void Test01() {		
 		System.out.println("I am in Test01 - Thread ID = " + Thread.currentThread().getId() );			
-	}			
+	}
 }
